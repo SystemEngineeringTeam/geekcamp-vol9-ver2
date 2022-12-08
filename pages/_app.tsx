642ec7d0db90b5e89bad93c4e5f00b6a9cbe7c56
harutiro <harutiro2027@outlook.jp>
2022-12-09 08:31:03 +0900

    update:ヘッダーがかぶるのを対策した。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "./_app.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
