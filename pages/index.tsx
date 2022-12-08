27cecbd7e313960912c41a444b33e51b848b6527
harutiro <harutiro2027@outlook.jp>
2022-12-09 01:13:32 +0900

    update:gridの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Card from "./card";
+import { Grid } from "./grid";
 
 export default function Home() {
   return (
     <>
-      <Card num={0}>C</Card>
+      <Grid/>
     </>
   )
 }
