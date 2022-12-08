cd95a252c8769b28303f85dd555c13c434583f9e
harutiro <harutiro2027@outlook.jp>
2022-12-09 01:29:22 +0900

    update: headerの完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Card from "./card";
 import { Grid } from "./grid";
+import  Header  from "./header";
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <Grid/>
     </>
   )
