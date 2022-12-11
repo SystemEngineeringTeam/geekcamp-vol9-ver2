df7d6ad321da9608ae3d2cf92ac3f052368bfc2a
MukPan <k22005kk@aitech.ac.jp>
2022-12-11 09:36:29 +0900

    add:ロードアイコンの追加

--- pages/startPage.tsx
+++ pages/startPage.tsx
@@ -1,15 +1,34 @@
 import { Grid } from "./grid";
 import  Header  from "./header";
 import Loader from 'react-loader-spinner';
+import { Player, PlayerEvent } from "@lottiefiles/react-lottie-player";
+import { useRef } from "react";
 
 export default function StartPage(){
+    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    const playerRef = useRef<Player>(null);
+
+    //レンダリング後(ロード後)の処理
+    const endLoading = (event:PlayerEvent) => {
+        if (event === "play") {
+            (async () => {
+                await sleep(5000); //５秒表示
+                const loadingIcon = document.getElementsByClassName("loading") as HTMLCollectionOf<HTMLElement>;
+                playerRef.current!.stop();
+                loadingIcon[0].style.display = "none"; //ロードアイコンを消す
+                
+            })();
+        }
+    }
+    
     const startPage:{[key:string]:string} = {
         height: "100%",
         width: "100%",
         zIndex: "1",
         position: "absolute",
-        backgroundColor: "grey",
-        opacity: "0.6"
+        backgroundColor: "#FFEBC1",
+        opacity: "0.9"
         
     }
 
@@ -23,6 +42,17 @@ export default function StartPage(){
         zIndex: "2",
         position: "absolute"
     }
+    
+    const loadingStyle:{[key:string]:string} = {
+        height: "500px",
+        width: "500px",
+        top: "50%",
+        left: "50%",
+        marginTop: "-250px",
+        marginLeft: "-250px",
+        position: "absolute",
+        zIndex: "3",
+    };
 
     const removeStartPage = () : void => {
         const target = document.getElementsByClassName("disappered") as HTMLCollectionOf<HTMLElement>;
@@ -33,6 +63,15 @@ export default function StartPage(){
 
     return( 
         <>
+            <Player
+            autoplay
+            loop
+            className="loading"
+            src="https://assets1.lottiefiles.com/datafiles/DlRM2jtACyr4IX1u6l5rqtW1QWZKLCkNoBIXWeyH/loading.json"
+            style={loadingStyle}
+            ref={playerRef}
+            onEvent={endLoading}
+            />
             <button style={styleButton} onClick={removeStartPage} className="disappered">スタート</button>
             <div style={startPage} className="disappered"></div>
             <div style={{zIndex: "0",position: "absolute" , width: "100%"}}>
